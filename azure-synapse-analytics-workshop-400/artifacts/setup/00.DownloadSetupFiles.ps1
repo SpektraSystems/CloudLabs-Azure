52f9a73478991bbb05a3442cc8e885f1f9550e19
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2020-04-30 04:33:00 -0700

    Update 00.DownloadSetupFiles.ps1

--- azure-synapse-analytics-workshop-400/artifacts/setup/00.DownloadSetupFiles.ps1
+++ azure-synapse-analytics-workshop-400/artifacts/setup/00.DownloadSetupFiles.ps1
@@ -2,7 +2,9 @@ CD C:\LabFiles
 
 Install-Module -Name "Az" -AllowClobber
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SpektraSystems/CloudLabs-Azure/master/azure-synapse-analytics-workshop-400/artifacts/setup/01.Setup-Storage.ps1" -OutFile "C:\LabFiles\01.Setup-Storage.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SpektraSystems/CloudLabs-Azure/master/azure-synapse-analytics-workshop-400/artifacts/setup/01.SetupPermissions.ps1" -OutFile "C:\LabFiles\01.SetupPermissions.ps1"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SpektraSystems/CloudLabs-Azure/master/azure-synapse-analytics-workshop-400/artifacts/setup/02.Setup-Storage.ps1" -OutFile "C:\LabFiles\02.Setup-Storage.ps1"
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SpektraSystems/CloudLabs-Azure/master/azure-synapse-analytics-workshop-400/artifacts/setup/51.ValidateStorageData.ps1" -OutFile "C:\LabFiles\51.ValidateStorageData.ps1"
 
