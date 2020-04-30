637771f6e2de1ca8b0bc6190b4bf0a55a8a3c105
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2020-04-30 04:32:04 -0700

    Adding script to setup permissions

---
+++ azure-synapse-analytics-workshop-400/artifacts/setup/01.SetupPermissions.ps1
@@ -0,0 +1,43 @@
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*L400*" }).ResourceGroupName
+$uniqueId =  (Get-AzResourceGroup -Name $resourceGroupName).Tags["DeploymentId"]
+$subscriptionId = (Get-AzContext).Subscription.Id
+$tenantId = (Get-AzContext).Tenant.Id
+$keyVaultName = "asakeyvault$($uniqueId)"
+$workspaceName = "asaworkspace$($uniqueId)"
+$amlWorkspaceName = "amlworkspace$($uniqueId)"
+$blobStorageAccountName = "asastore$($uniqueId)"
+
+$userPrincipalName = (Get-AzContext).Account.Id
+$userObjectId = (Get-AzADUser -UserPrincipalName (Get-AzContext).Account.Id).Id
+$workspaceId  = (Get-AzADServicePrincipal -DisplayName $workspaceName).Id
+$amlWorkspaceId  = (Get-AzADServicePrincipal -DisplayName $amlWorkspaceName).Id
+
+Set-AzKeyVaultAccessPolicy -VaultName  $keyVaultName -UserPrincipalName $userPrincipalName -PermissionsToSecrets get,list,set,delete -PassThr
+Set-AzKeyVaultAccessPolicy -VaultName  $keyVaultName -ObjectId $workspaceId -PermissionsToSecrets get,list -PassThr
+Set-AzKeyVaultAccessPolicy -VaultName  $keyVaultName -ObjectId $amlWorkspaceId -PermissionsToKeys get,list,update,create,import,delete,recover,backup,restore,decrypt,encrypt,wrapkey,unwrapkey,verify,sign `
+        -PermissionsToCertificates Get,List,Update,Create,Import,Delete,Recover,Backup,Restore,ManageContacts,ManageIssuers,GetIssuers,ListIssuers,SetIssuers,DeleteIssuers `
+        -PermissionsToSecrets get,list,set,delete,recover,backup,restore -PassThr
+
+
+$policies = (Get-AzKeyVault -VaultName $keyVaultName -ResourceGroupName $resourceGroupName).AccessPolicies
+foreach($policy in $policies)
+{
+    $policy.DisplayName
+    "CERT: " + $policy.PermissionsToCertificatesStr
+    "KEYS: " + $policy.PermissionsToKeysStr
+    "SECRETS: " + $policy.PermissionsToSecretsStr
+}
+
+New-AzRoleAssignment -ResourceGroupName $resourceGroupName -ResourceName $blobStorageAccountName -ResourceType "Microsoft.Storage/storageAccounts" -SignInName $userPrincipalName -RoleDefinitionName "Storage Blob Data Contributor"
+
+Logout-AzAccount
